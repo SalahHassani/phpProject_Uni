3c36b258b5c12652615a0b568b0bbf1d6ac2a264
salahHassani <shassani908@gmail.com>
2023-12-06 23:46:13 +0100

    third commit

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ fetch("./PokemonData.json")
       //   console.log(ele);
       let newCard = document.createElement("div");
       let html = `      <div class="card">
-      <div class="cardRow pokemonCardImage">
+      <div class="cardRow pokemonCardImage ${data[ele].type}">
         <img
           src="${data[ele].image}"
           alt="no image found"
@@ -34,13 +34,13 @@ fetch("./PokemonData.json")
         <div class="pokemoneStatus">
           <div><img src="./images/health.png" alt="no image found" /> ${data[ele].hp}</div>
           <div>
-            <img src="./images/defance.png" alt="no image found" /> 20
+            <img src="./images/defance.png" alt="no image found" /> ${data[ele].defense}
           </div>
-          <div><img src="./images/attack.png" alt="no image found" /> 30</div>
+          <div><img src="./images/attack.png" alt="no image found" /> ${data[ele].attack}</div>
         </div>
       </div>
       <div class="cardRow pokemoneCardPrice">
-        <p><img src="./images/moneyBag.png" alt="no image found" /> 160</p>
+        <p><img src="./images/moneyBag.png" alt="no image found" /> ${data[ele].price}</p>
       </div>
     </div>`;
 
